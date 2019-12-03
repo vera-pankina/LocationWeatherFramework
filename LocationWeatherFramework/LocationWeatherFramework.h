e6579d73b2c837af3d4af2fc05a78d2ef2baa974
Vera Pankina <vera.pankina@bsc-ideas.com>
2019-12-04 01:17:30 +0300

    Initial Commit

---
+++ LocationWeatherFramework/LocationWeatherFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocationWeatherFramework.h
+//  LocationWeatherFramework
+//
+//  Created by vpankina on 04/12/2019.
+//  Copyright © 2019 vpankina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocationWeatherFramework.
+FOUNDATION_EXPORT double LocationWeatherFrameworkVersionNumber;
+
+//! Project version string for LocationWeatherFramework.
+FOUNDATION_EXPORT const unsigned char LocationWeatherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocationWeatherFramework/PublicHeader.h>
+
+
